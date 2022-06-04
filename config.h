7ffc80341567b193794926631a404c3218405393
ok <ok@YOGA530.not>
2022-06-04 17:37:53 +0000

    add keepassxc tag mask

--- config.h
+++ config.h
@@ -35,10 +35,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "KeePassXC",      NULL,     NULL,       1 << 6,         0,          0,           0,        -1 },
 	{ TERMCLASS,      NULL,     NULL,       0,         0,          1,           0,        -1 },
+	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 };
 
 /* layout(s) */
